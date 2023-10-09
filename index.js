47e93ee05bb1af00f7aa43fec4fcbab08e582179
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-09 22:35:55 +0200

    tweet data model

---
+++ index.js
@@ -0,0 +1,32 @@
+// 1- modelo de datos con los tweets
+const tweets = [
+  {
+    handler: 'user1',
+    date: new Date().toISOString(),
+    message: 'Contenido del tweet 1',
+    likes: 40,
+  },
+  {
+    handler: 'user2',
+    date: new Date().toISOString(),
+    message: 'Contenido del tweet 2',
+    likes: 20,
+  },
+  {
+    handler: 'user3',
+    date: new Date().toISOString(),
+    message: 'Contenido del tweet 3',
+    likes: 430,
+  },
+  {
+    handler: 'user4',
+    date: new Date().toISOString(),
+    message: 'Contenido del tweet 4',
+    likes: 10,
+  },
+];
+
+// 2- recorrer el array de tweets
+
+// 3- crear estructura de HTML con los datos de los tweets
+// 4- añadimos al DOM
