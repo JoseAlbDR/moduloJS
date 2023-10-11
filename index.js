7141c828f0bcea7c3e6525b1aa74f323eab86662
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-11 22:46:26 +0200

    feat: remove buttons

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
 import { tweetListController } from './tweet-list/tweetListController.js';
 import { notificationController } from './notifications/notificationsController.js';
 
+// const loadButton = document.getElementById('loadTweets');
+// const removeButton = document.getElementById('removeTweets');
+// const hideButton = document.getElementById('hideTweets');
 const tweetList = document.getElementById('tweets');
-const loadButton = document.getElementById('loadTweets');
-const removeButton = document.getElementById('removeTweets');
-const hideButton = document.getElementById('hideTweets');
 const notifications = document.getElementById('notifications');
-
 const showNotification = notificationController(notifications);
 
-const removeTweets = () => {
-  tweetList.innerHTML = '';
-};
-
-loadButton.addEventListener('click', () => {
-  removeTweets();
-  tweetList.classList.remove('hidden');
-  tweetListController(tweetList);
-});
-
-removeButton.addEventListener('click', removeTweets);
-
-hideButton.addEventListener('click', () => {
-  tweetList.classList.toggle('hidden');
-});
+tweetListController(tweetList);
 
 tweetList.addEventListener('tweetsLoaded', (event) => {
-  // console.log('los tweets han sido cargados');
-  console.log(event.detail);
   showNotification(event.detail.message, event.detail.type);
 });
+// const removeTweets = () => {
+//   tweetList.innerHTML = '';
+// };
+
+// loadButton.addEventListener('click', () => {
+//   removeTweets();
+//   tweetList.classList.remove('hidden');
+// });
+
+// removeButton.addEventListener('click', removeTweets);
+
+// hideButton.addEventListener('click', () => {
+//   tweetList.classList.toggle('hidden');
+// });
