b4ec99c6e0567fd5e1a98212dcce6b9c4ee25d91
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-10-17 20:43:47 +0200

    feat: logout button

--- session/sessionController.js
+++ session/sessionController.js
@@ -1,9 +1,18 @@
-import { buildSession } from './sessionView.js';
+import { buildLogout, buildSession } from './sessionView.js';
 
 export const sessionController = (nav) => {
-  const token = localStorage.getItem('accessToken');
-
-  if (!token) {
+  if (isUserLoggedIn()) {
+    nav.innerHTML = buildLogout();
+    const logoutBtn = nav.querySelector('button');
+    logoutBtn.addEventListener('click', () => {
+      localStorage.removeItem('accessToken');
+      window.location.reload();
+    });
+  } else {
     nav.innerHTML = buildSession();
   }
+};
+
+const isUserLoggedIn = () => {
+  return localStorage.getItem('accessToken');
 };
--- session/sessionView.js
+++ session/sessionView.js
@@ -8,3 +8,9 @@ export const buildSession = () => {
   </ul>
   `;
 };
+
+export const buildLogout = () => {
+  return `
+  <button id="logoutBtn">Logout</button>
+  `;
+};
